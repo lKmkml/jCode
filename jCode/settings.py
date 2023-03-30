0e426da65de1bb28bf5a1bc42ccadb4a1b2135a4
Pipatpong Thadapitakporn <51953443+lKmkml@users.noreply.github.com>
2023-03-30 22:16:00 +0700

    Update settings.py

--- jCode/settings.py
+++ jCode/settings.py
@@ -87,13 +87,23 @@ WSGI_APPLICATION = 'jCode.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'root',
+        'PASSWORD': 'APPc0de@2018y',
+        'HOST': '206.189.95.6',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
